bdd67abbebc7f8e1dd03a8142372e0ebb31fe5d0
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-10 16:34:44 -0600

    build: model relationships and validations

---
+++ app/models/invoice_item.rb
@@ -0,0 +1,9 @@
+class InvoiceItem < ApplicationRecord
+  belongs_to :invoice
+  belongs_to :item
+
+  validates_presence_of :quantity,
+                        :unit_price,
+                        :item_id,
+                        :invoice_id
+end
