574b7745c8b36bb048404d43c6d591382b40373d
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-11 11:21:08 -0600

    feat: add #index for merchants/:id/items

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -2,7 +2,9 @@ module Api
   module V1
     module Merchants
       class ItemsController < ApplicationController
-
+        def index
+          render json: ItemSerializer.new(MerchantItemsFacade.find_by_merch_id(params[:merchant_id]))
+        end
       end
     end
   end
