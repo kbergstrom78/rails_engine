936059cd2dd8fe596f0dc4d6ba56a7c693e57267
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-11 15:19:04 -0600

    build: add route for items#create

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: %i[index show] do
+      resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: 'merchants/items'
       end
-      resources :items, only: %i[index show] do
+      resources :items, only: [:index, :show, :create] do
       end
     end
   end
