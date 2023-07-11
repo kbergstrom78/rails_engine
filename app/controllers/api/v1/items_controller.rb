c780854df58a5beba9714ce71102a6714c0f368a
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-11 14:31:25 -0600

    Perf: rubocop sweep

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class ItemsController < ApplicationController
       def index
         render json: ItemSerializer.new(Item.all)
+      end
+
+      def show
+        render json: ItemSerializer.new(Item.find(params[:id]))
       end
     end
   end
--- app/facades/merchant_items_facade.rb
+++ app/facades/merchant_items_facade.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class MerchantItemsFacade
   def self.find_by_merch_id(merch_id)
     Merchant.find(merch_id).items
