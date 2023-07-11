d3a4a187a5d66a6873426c9c3c5e7ae2bf50d2b0
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-11 10:56:58 -0600

    test: add test for /merchants/#{:id}

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -18,5 +18,19 @@ RSpec.describe 'Merchants API' do
         expect(merchant[:attributes][:name]).to be_a(String)
       end
     end
+
+    it 'can get one merchant by its id' do
+      id = create(:merchant).id
+
+      get "/api/v1/merchants/#{id}"
+
+      merchant_info = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
+      expect(merchant_info[:data][:attributes]).to have_key(:name)
+      expect(merchant_info[:data][:attributes][:name]).to be_a(String)
+      expect(merchant_info[:data][:id]).to eq(id.to_s)
+    end
   end
 end
