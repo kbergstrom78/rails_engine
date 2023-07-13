55320eb7bfb7f719cb93870de5ee224a3092c211
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-12 18:09:56 -0600

    test: add test for item validations and class method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,6 +5,9 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to(:merchant) }
+    it { should have_many(:invoice_items) }
+    it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:customers).through(:invoices) }
   end
 
   describe 'validations' do
@@ -18,7 +21,25 @@ RSpec.describe Item, type: :model do
     before :each do
       @merchant = create(:merchant)
       @merchant2 = create(:merchant)
-      @item = create(:item, merchant_id: @merchant.id)
+      @customer = create(:customer)
+
+      @item1 = create(:item, merchant: @merchant)
+      @item2 = create(:item, merchant: @merchant)
+
+      @invoice1 = create(:invoice, merchant: @merchant, customer: @customer)
+      @invoice2 = create(:invoice, merchant: @merchant, customer: @customer)
+
+      create(:invoice_item, item: @item1, invoice: @invoice1, quantity: 1, unit_price: 9.99)
+      create(:invoice_item, item: @item1, invoice: @invoice2, quantity: 1, unit_price: 9.99)
+      create(:invoice_item, item: @item2, invoice: @invoice2, quantity: 1, unit_price: 9.99)
+    end
+
+    describe "#invoice_destroy" do
+      it 'destroys invoices if the item is the only one on it' do
+        expect { @item1.invoice_destroy }.to change { Invoice.count }.by(-1)
+        expect(Invoice.find_by(id: @invoice1.id)).to be_nil
+        expect(Invoice.find_by(id: @invoice2.id)).to_not be_nil
+      end
     end
 
     context 'when item and merchant exist' do
@@ -30,7 +51,7 @@ RSpec.describe Item, type: :model do
           merchant_id: @merchant2.id
         }
 
-        updated_item, error = Item.update_with_merchant_check(@item.id, @merchant2.id, params)
+        updated_item, error = Item.update_with_merchant_check(@item1.id, @merchant2.id, params)
 
         expect(updated_item.name).to eq(params[:name])
         expect(updated_item.description).to eq(params[:description])
@@ -42,7 +63,7 @@ RSpec.describe Item, type: :model do
 
     context 'when item does not exist' do
       it 'returns nil and an error message' do
-        non_existent_id = @item.id + 1000
+        non_existent_id = @item1.id + 1000
         updated_item, error = Item.update_with_merchant_check(non_existent_id, @merchant2.id, {})
 
         expect(updated_item).to be_nil
@@ -55,7 +76,7 @@ RSpec.describe Item, type: :model do
         non_existent_merchant_id = @merchant.id + 1000
         params = { merchant_id: non_existent_merchant_id }
 
-        updated_item, error = Item.update_with_merchant_check(@item.id, non_existent_merchant_id, params)
+        updated_item, error = Item.update_with_merchant_check(@item1.id, non_existent_merchant_id, params)
 
         expect(updated_item).to be_nil
         expect(error).to eq('Merchant not found')
