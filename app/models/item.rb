df3974e24290385487b852f4322a8255efe4749e
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-12 18:11:46 -0600

    refact: add relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@
 
 class Item < ApplicationRecord
   belongs_to :merchant
+  has_many :invoice_items, dependent: :delete_all
+  has_many :invoices, through: :invoice_items
+  has_many :customers, through: :invoices
 
   validates_presence_of :name,
                         :unit_price,
@@ -17,5 +20,12 @@ class Item < ApplicationRecord
 
     item.update!(item_params)
     [item, nil]
+  end
+
+  def invoice_destroy
+    self.invoice_items.each do |invoice_item|
+    invoice = Invoice.find(invoice_item.invoice_id)
+    invoice.destroy if invoice.invoice_items.count == 1
+    end
   end
 end
