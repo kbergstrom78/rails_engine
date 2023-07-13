c9daecb4ce41e05e324a23423cef79324d4b4bbd
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-13 14:21:28 -0600

    refact: update items/merchant controller for rescue_from AR

--- app/controllers/api/v1/items/merchant_controller.rb
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -2,9 +2,21 @@ module Api
   module V1
     module Items
       class MerchantController < ApplicationController
+      rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
         def index
-          render json: MerchantSerializer.new(Item.find(params[:item_id]).merchant)
+          item = Item.find(params[:item_id])
+          merchant = item.merchant
+          render json: MerchantSerializer.new(merchant)
         end
+
+        private
+
+        def record_not_found
+          render status: 404, json: { error: "Item not Found"}
+        end
+
+
       end
     end
   end
