ce32d380904e66a13006486851ff2674c79798ff
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-13 12:59:26 -0600

    refact: add rescue for ActiveRecord error, merchant not found

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,7 +8,12 @@ module Api
       end
 
       def show
-        render json: MerchantSerializer.new(Merchant.find(params[:id]))
+        begin
+          merchant = Merchant.find(params[:id])
+          render json: MerchantSerializer.new(Merchant.find(params[:id]))
+        rescue ActiveRecord::RecordNotFound
+          render json: { error: "Couldn't find merchant with 'id'=#{params[:id]}" }, status: :not_found
+        end
       end
     end
   end
