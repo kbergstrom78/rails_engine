290da06c16661f9493ed2851ea094bd123cdf889
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-13 17:11:08 -0600

    build: add search_by class method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,8 @@ class Merchant < ApplicationRecord
   has_many :items
 
   validates_presence_of :name
+
+  def self.search_by_name(name_fragment)
+    where("name ILIKE '%#{name_fragment}%'").order(:name)
+  end
 end
